309fa3695dadfa6fcca40e2b14e4d0034d82c5e6
Ulidaget <62191623+Ulidaget@users.noreply.github.com>
2024-06-14 16:53:03 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,20 +6,7 @@ import requests
 
 # Write directly to the app
 st.title(" :cup_with_straw: Customize Your Smoothie! :cup_with_straw: ")
-st.write(
-    """
-    Choose the fruits you want in your custom Smoothie!!
-    """
-    # """Replace this example with your own code!
-    # **And if you're new to Streamlit,** check
-    # out our easy-to-follow guides at
-    # [docs.streamlit.io](https://docs.streamlit.io).
-    # """
-)
-
-# option = st.selectbox('Whats your favourite fruit?', ('Banana','Strawberries','Peaches'))
-
-# st.write('Your favorite fruite is:', option)
+st.write( """ Choose the fruits you want in your custom Smoothie!! """)
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write('The name on your Smoothie will be:', name_on_order)
@@ -27,7 +14,8 @@ st.write('The name on your Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake") 
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 ingredients_list = st.multiselect('Chose up to 5 ingredients:', my_dataframe, max_selections=5)
 
 if ingredients_list:
