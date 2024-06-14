b8f1a49f2e49b1bde4f57c1e81e902afbf199e90
Ulidaget <62191623+Ulidaget@users.noreply.github.com>
2024-06-14 17:07:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,9 +30,14 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
+        
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.sunheader(fruit_chosen + 'Nutrition Information ')
         fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_chosen}")
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        #fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
